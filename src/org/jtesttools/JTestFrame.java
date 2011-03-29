0fb7fc7f60f129574c0e17a1109ee2edc9f87188
Andy Brown <adb1413@rit.edu>
2011-03-28 23:13:20 -0400

    Documented JTestFrame more

--- src/org/jtesttools/JTestFrame.java
+++ src/org/jtesttools/JTestFrame.java
@@ -42,17 +42,24 @@ public class JTestFrame extends JFrame {
 	}
 	
 	/**
-	 * Shows frame until it's closed, then disposes of it.
+	 * Shows a frame until it's closed, then disposes of it.
+	 * 
+	 * @param frame JFrame to show, may be generic
 	 */
 	public static void run(final JFrame frame) {
 	   run(frame, -1);
 	}
 	
 	/**
-	 * Shows frame for time milliseconds, then disposes of it.
+	 * Shows a frame for a certain amount of time, then disposes of it.
+	 * 
+    * @param frame JFrame to show, may be generic
+    * @param time Number of milliseconds to wait for frame to close
+    * @throws RuntimeException if interrupted while waiting
 	 */
 	public static void run(final JFrame frame, long time) {
 	   
+	   // Show the frame
       SwingUtilities.invokeLater(new Runnable() {
          @Override
          public void run() {
@@ -61,6 +68,7 @@ public class JTestFrame extends JFrame {
          }
       });
       
+      // Wait for it to close
       synchronized (frame) {
    	   try {
    	      if (time < 0) {
@@ -73,6 +81,7 @@ public class JTestFrame extends JFrame {
    	   }
       }
       
+      // Dispose of frame
       frame.setVisible(false);
       frame.dispose();
 	}
@@ -81,6 +90,9 @@ public class JTestFrame extends JFrame {
    // Helpers
    //
 	
+	/**
+	 * Wakes up all threads waiting on the frame.
+	 */
 	private synchronized void wake() {
 	   notifyAll();
 	}
@@ -89,8 +101,14 @@ public class JTestFrame extends JFrame {
    // Nested classes
    //
    
+	/**
+	 * Observer of key events.
+	 */
    class KeyObserver extends KeyAdapter {
       
+      /**
+       * Wakes up waiting threads when the Escape key is pressed.
+       */
       @Override
       public void keyReleased(KeyEvent event) {
          if (event.getKeyCode() == KeyEvent.VK_ESCAPE) {
@@ -99,8 +117,14 @@ public class JTestFrame extends JFrame {
       }
    }
    
+   /**
+    * Observer of window events.
+    */
    class WindowObserver extends WindowAdapter {
       
+      /**
+       * Wakes up waiting threads when the window is closed.
+       */
       @Override
       public void windowClosing(WindowEvent event) {
          wake();
